2cb99ce4d2019fa9b816208d48b19750b69504b7
chhak0503 <chhak0503@gmail.com>
2025-07-31 11:44:45 +0900

    add ch08

--- ch08/src/main/java/sub1/WelcomeServlet.java
+++ ch08/src/main/java/sub1/WelcomeServlet.java
@@ -8,20 +8,20 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class WelcomeServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	public void init() throws ServletException {
 		// 서블릿이 최초 생성될 때 실행되는 초기화 메서드 
-		System.out.println("helloServlet init...");
+		System.out.println("WelcomeServlet init...");
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 클라이언트 Get 요청 실행
-		System.out.println("HelloServlet get 요청!!!");
+		System.out.println("WelcomeServlet get 요청!!!");
 		
 		// HTML 출력
 		resp.setContentType("text/html;charset=UTF-8");
@@ -30,10 +30,10 @@ public class HelloServlet extends HttpServlet {
 		writer.println("<html>");
 		writer.println("<head>");
 		writer.println("<meta charset='UTF-8'/>");
-		writer.println("<title>HelloServlet</title>");
+		writer.println("<title>WelcomeServlet</title>");
 		writer.println("</head>");
 		writer.println("<body>");
-		writer.println("<h3>HelloServlet</h3>");
+		writer.println("<h3>WelcomeServlet</h3>");
 		writer.println("<a href='/ch08/Servlet.jsp'>서블릿 메인</a><br/>");
 		writer.println("<a href='/ch08/hello.do'>Hello Servlet</a><br/>");
 		writer.println("<a href='/ch08/welcome.do'>Welcome Servlet</a><br/>");
@@ -45,16 +45,7 @@ public class HelloServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 클라이언트 Post 요청 실행
-		System.out.println("HelloServlet post 요청!!!");
-		
-		String id = req.getParameter("id");
-		String pw = req.getParameter("pw");
-		
-		System.out.println("id : " + id);
-		System.out.println("pw : " + pw);
-		
-		resp.sendRedirect("/ch08/greeting.do");
+		System.out.println("WelcomeServlet post 요청!!!");
 	}
 	
 	
