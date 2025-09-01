b3ad6e028ba89ecfda6a5952420522c6fcd92b0b
chhak0503 <chhak0503@gmail.com>
2025-09-01 17:26:23 +0900

    add jboard

--- jboard/src/main/java/jboard/dao/TermsDAO.java
+++ jboard/src/main/java/jboard/dao/TermsDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jboard.dto.ArticleDTO;
 import jboard.dto.TermsDTO;
 import jboard.util.DBHelper;
+import jboard.util.Sql;
 
 public class TermsDAO extends DBHelper {
 	
@@ -20,7 +21,28 @@ public class TermsDAO extends DBHelper {
 	}
 	
 	public TermsDTO select(int no) {
-		return null;
+		
+		TermsDTO dto = null;
+		
+		try {
+			conn = getConnection();
+			
+			psmt = conn.prepareStatement(Sql.SELECT_TERMS);
+			psmt.setInt(1, no);
+			
+			rs = psmt.executeQuery();
+			
+			if(rs.next()) {
+				dto = new TermsDTO();
+				dto.setNo(rs.getInt(1));
+				dto.setTerms(rs.getString(2));
+				dto.setPrivacy(rs.getString(3));
+			}
+			closeAll();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return dto;
 	}
 	
 	public List<TermsDTO> selectAll() {
