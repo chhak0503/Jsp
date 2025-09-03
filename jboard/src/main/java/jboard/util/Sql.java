99389ad01d6b1f4aef73be4e6a9a5c3996065d27
chhak0503 <chhak0503@gmail.com>
2025-09-03 09:28:05 +0900

    add jboard

--- jboard/src/main/java/jboard/util/Sql.java
+++ jboard/src/main/java/jboard/util/Sql.java
@@ -14,6 +14,8 @@ public class Sql {
 	public static final String WHERE_EMAIL = "WHERE EMAIL=?";
 	public static final String WHERE_HP   = "WHERE HP=?";
 	
+	public static final String SELECT_USER_BY_PASS = "SELECT * FROM TB_USER WHERE USID=? AND PASS=STANDARD_HASH(?, 'SHA256')";
+	
 	
 	public static final String INSERT_USER = "INSERT INTO TB_USER (USID, PASS, US_NAME, NICK, EMAIL, HP, ZIP, ADDR1, ADDR2, REG_IP, REG_DATE) "
 											+ "VALUES (?,STANDARD_HASH(?, 'SHA256'),?,?,?,?,?,?,?,?,SYSDATE)";
